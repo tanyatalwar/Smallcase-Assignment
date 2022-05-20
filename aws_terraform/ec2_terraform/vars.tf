3f3c5955202c6f0e08398aa67357940658df4887
tanyatalwar <tanyatalwar8@gmail.com>
2022-05-20 17:40:29 +0530

    Update vars.tf

--- aws_terraform/ec2_terraform/vars.tf
+++ aws_terraform/ec2_terraform/vars.tf
@@ -1,23 +1,13 @@
----------------------------------
-new var file
-
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_ACCESS_KEY" {}
-variable "AWS_ACCESS_KEY" {}
-
-
-
-
-
-
-
----------------------------------
-variable "access_key_cred" {
-    type = "string"    // export TF_VAR_access_key_cred="xxxxxxx"
+variable "AWS_ACCESS_KEY" {
+     type = "string" // export TF_VAR_AWS_ACCESS_KEY="xxxxxxx"
 }
 
-variable "secret_key_cred" {
-    type = "string"     // export TF_VAR_secret_key_cred="xxxxxxx"
+variable "AWS_REGION" {
+     type = "string" // export TF_VAR_AWS_REGION="xxxxxxx"
+}
+
+variable "AWS_SECRET_KEY" {
+     type = "string" // export TF_VAR_AWS_SECRET_KEY="xxxxxxx"
 }
 
 variable "image_id" {
@@ -28,25 +18,6 @@ variable "image_id" {
 variable "instance_type" {
      default = "t2.micro"
      type = "string"
-}
-
-variable "instanceTenancy" {
-    default = "default"
-}
-
-variable "dnsSupport" {
-    default = true
-}
-variable "dnsHostNames" {
-    default = true
-}
-
-variable "subnetCIDRblock" {
-    default = "10.0.1.0/24"
-}
-
-variable "mapPublicIP" {
-    default = true
 }
 
 variable "availabilityZone" {
